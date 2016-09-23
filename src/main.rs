3d51042b4f3f9023ddd3edf6232c1359f2f65134
Marc Brinkmann <git@marcbrinkmann.de>
2016-09-23 11:18:30 +0200

    New-style blinky complete.

--- src/main.rs
+++ src/main.rs
@@ -5,49 +5,43 @@
 extern crate embedded;
 
 use embedded::components::gpio;
-use embedded::base::volatile::{Volatile, VolatileStruct};
 use embedded::util::delay;
 
-// on AHB1, RCC is at 0x4002 3800 - 0x4002 3BFF RCC (DM00071990, pg. 86)
-const AHB1_RCC_BASE: u32 = 0x4002_3800;
-
-// DM00031020.pdf, pg 180
-const RCC_AHB1ENR: u32 = AHB1_RCC_BASE + 0x30;
-// to enable GPIOG, we need to toggle the 6th bit
-const RCC_AHB1ENR_GPIOG_EN: u32 = 0b0100_0000;
 
 board!(stm32f429i, {
 });
 
-#[repr(C)]
-struct SingleItemReg<T> {
-    reg: Volatile<T>,
-}
-
-impl<T> VolatileStruct for SingleItemReg<T> {}
-
 fn main(hw: Hardware) {
-    let mut rcc_ahb1enr =
-        unsafe { SingleItemReg::from_ptr(RCC_AHB1ENR as *mut SingleItemReg<u32>) };
-    let Hardware { gpio_g, .. } = hw;
+    let Hardware { gpio_g, rcc, .. } = hw;
 
-    // enable clock on gpio register
-    rcc_ahb1enr.reg |= RCC_AHB1ENR_GPIOG_EN;
+    // power on gpios
+    rcc.enable_all_gpio_ports();
 
+    // setup output pin
     let (mut gpio_ctrl, gpio_pins) = gpio_g.split();
+    let gpio::GpioPins { pin_13, pin_14, .. } = gpio_pins;
 
-    let gpio::GpioPins { pin_13, .. } = gpio_pins;
-
+    // FIXME: would a builder-pattern here be nicer?
     let mut p13 = gpio_ctrl.to_write(pin_13,
                                      gpio::Type::PushPull,
                                      gpio::Speed::Low,
                                      gpio::Resistor::NoPull);
 
+
+    let mut p14 = gpio_ctrl.to_write(pin_14,
+                                     gpio::Type::PushPull,
+                                     gpio::Speed::Low,
+                                     gpio::Resistor::NoPull);
+
+
+
     loop {
-        p13.set(true);
+        p13.set(false);
+        p14.set(true);
         delay(INITIAL_CPU_FREQ);
 
-        p13.set(false);
+        p13.set(true);
+        p14.set(false);
         delay(INITIAL_CPU_FREQ);
     }
 }
